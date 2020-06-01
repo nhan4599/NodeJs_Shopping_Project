acbf93f1ad19c2e35a30fdd9a07f399a8a3e0575
nguyen tri nhan ho <nhan01628483955@gmail.com>
2020-06-01 11:28:49 +0700

    deploy

--- controller/admin.js
+++ controller/admin.js
@@ -2,4 +2,12 @@ var express = require('express');
 
 var router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send('test');
+});
+
+router.get('/products', (req, res) => {
+    res.render('admin/product');
+});
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 var express = require('express');
-var parser = require('body-parser');
-var session = require('express-session');
+// var parser = require('body-parser');
+// var session = require('express-session');
 
 var app = express();
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
-app.use(parser.urlencoded({ extended: true }));
-app.use(parser.json());
-app.use(session({ secret: '123456' }));
-app.use((req, res, next) => {
-    res.locals.session = req.session;
-    next();
+app.set('view engine', 'ejs');
+
+// app.use(parser.urlencoded({ extended: true }));
+// app.use(parser.json());
+// app.use(session({ secret: '123456' }));
+// app.use((req, res, next) => {
+//     res.locals.session = req.session;
+//     next();
+// });
+
+app.get('/assets/css/:fileName', (req, res) => {
+    res.sendFile(`${__dirname}/assets/css/${req.params.fileName}`);
+});
+
+app.get('/assets/js/:fileName', (req, res) => {
+    res.sendFile(`${__dirname}/assets/js/${req.params.fileName}`);
+});
+
+app.get('/assets/fonts/:fileName', (req, res) => {
+    res.sendFile(`${__dirname}/assets/fonts/${req.params.fileName}`);
 });
 
 app.use('/', require('./controller/customer'));
