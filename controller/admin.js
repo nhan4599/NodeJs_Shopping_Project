76549546494da5ff99ca142daca325c921c5b365
nguyen tri nhan ho <nhan01628483955@gmail.com>
2020-06-06 22:14:08 +0700

    doing product

--- controller/admin.js
+++ controller/admin.js
@@ -35,6 +35,11 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.post('/logout', (req, res) => {
+    delete req.session.admin;
+    res.redirect('/admin/login');
+});
+
 router.get('/listproducts', (req, res) => {
     res.render('admin/listproducts');
 });
@@ -58,13 +63,67 @@ router.get('/listcustomers', (req, res) => {
 router.get('/createproduct', (req, res) => {
     var promises = [db.GetCategoryList(), db.GetManufacturerList(), db.GetSegmentList(), db.GetProductStateList()];
     Promise.all(promises).then(values => res.render('admin/createproduct', {
-                        categories: values[0],
-                        manufacturers: values[1],
-                        segments: values[2],
-                        staties: values[3] }));
+        categories: values[0],
+        manufacturers: values[1],
+        segments: values[2],
+        staties: values[3]
+    }));
 });
 
 router.post('/temp', (req, res) => {
     res.status(200).send('ok');
 });
+
+router.post('/addproductmetadata', (req, res) => {
+    var name = req.body.name;
+    var inventory = req.body.inventory;
+    var price = req.body.price;
+    var category = req.body.category;
+    var state = req.body.state;
+    var segment = req.body.segment;
+    req.session.productInfo = {
+        name, inventory, price, category, state, segment
+    };
+    res.redirect('/admin/productconfig');
+});
+
+router.get('/productconfig', (req, res) => {
+    if (!req.session.productInfo) {
+        res.status(500).send('Yêu cầu không hợp lệ');
+    } else {
+        res.render('admin/createproductconfig');
+    }
+});
+
+router.post('/addproductimages', (req, res) => {
+    var ram = req.body.ram;
+    var screen = req.body.screen;
+    var rearCam = req.body.rearCam;
+    var frontCam = req.body.frontCam;
+    var os = req.body.os;
+    var storage = req.body.storage;
+    var sdType = req.body.sdType;
+    var sdSize = req.body.sdSize;
+    var pin = req.body.pin;
+    req.session.productInfo.config = {
+        ram, screen, rearCam, frontCam, os, storage, sdType, sdSize, pin
+    };
+    res.redirect('/admin/productimages');
+});
+
+router.get('/productimages', (req, res) => {
+    if (!req.session.productInfo || !req.session.productInfo.config) {
+        res.status(500).send('Yêu cầu không hợp lệ');
+    } else {
+        res.render('admin/createproductimages');
+    }
+});
+
+router.post('/createproduct', (req, res) => {
+    if (!req.session.productInfo || !req.session.productInfo.config)
+        res.status(500).send('Yêu cầu không hợp lệ');
+    else
+        res.send(req.session.productInfo);
+});
+
 module.exports = router;
