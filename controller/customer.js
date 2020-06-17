ea3df2616a8278277facd29538bc7a1ef5eaf210
187PM14008 - Ho Tan Minh Tam - K24T-PM1 <tam.187pm14008@vanlanguni.vn>
2020-06-17 12:08:12 +0700

    login customer

--- controller/customer.js
+++ controller/customer.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var db = require('../database');
+var multer = require('multer');
 
 var router = express.Router();
+var upload = multer({});
 
-router.get('/', (req, res) => {
-    res.render('home');
+router.get('/login', (req, res) => {
+    res.render('customer/login');
+});
+
+router.get(['/', '/home'], (req, res) => {
+    res.render('customer/home');
 });
 
 module.exports = router;
