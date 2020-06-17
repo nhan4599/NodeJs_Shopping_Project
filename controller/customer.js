11b9a050d027e948ac9bbc3a96f4072063cf14fb
187PM14008 - Ho Tan Minh Tam - K24T-PM1 <tam.187pm14008@vanlanguni.vn>
2020-06-17 20:39:37 +0700

    login customer

--- controller/customer.js
+++ controller/customer.js
@@ -13,4 +13,30 @@ router.get(['/', '/home'], (req, res) => {
     res.render('customer/home');
 });
 
+router.post('/login', async (req, res) => {
+    var username = req.body.username;
+    var pass = req.body.password;
+    var result = await db.AdminLogin(username, pass);
+    if (result.message) {
+        res.render('/login', { message: 'Unexpected error' });
+    }
+    else {
+        if (result.rowsAffected[0] === 1) {
+            req.session.admin = result.recordset[0];
+            res.redirect('/home');
+        } else {
+            res.render('/login', { message: 'Username and/or password is incorrect' });
+        }
+    }
+});
+
+router.post('/logout', (req, res) => {
+    delete req.session.admin;
+    res.redirect('/home');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('customer/signup');
+});
+
 module.exports = router;
